3338439e1cc8bcf304a3905c46ab11555a9aa64d
alex <fennyflop@gmail.com>
2022-09-14 23:39:47 +0300

    added color feedbach

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,25 +2,28 @@
 	import { tweened } from 'svelte/motion';
 	import { cubicOut } from 'svelte/easing';
 
-	let amount;
-
+	
 	let maxPrice = 2000;
 	let minPrice = 1000; 
+	
+	let currentPrice = 1300;
 
-	let currentPrice = 1200;
+	let amount = currentPrice;
 
 	const currentPriceWidth = tweened((currentPrice - minPrice) / (maxPrice - minPrice) * 100, {
 		duration: 400,
 		easing: cubicOut
 	});
 
-	
 	const askingPriceWidth = tweened((amount - minPrice) / (maxPrice - minPrice) * 100, {
 		duration: 400,
 		easing: cubicOut
 	});
 
+	$: status = !amount || amount < minPrice ? "error" : amount < minPrice ? "error" : amount < currentPrice ? "low" : amount === currentPrice ? "normal" : "best";
+
 	$: {
+		console.log(status);
 		askingPriceWidth.set(amount < minPrice ? 0 : amount > maxPrice ? 100 : (amount - minPrice) / (maxPrice - minPrice) * 100);
 	}
 
@@ -36,56 +39,97 @@
 </svelte:head>
 
 <main class="main">
-	<section>
-		<p class="title">Ready to pay:</p>
-		<input bind:value={amount} class="input" type="number" placeholder="Amount..." /> 
+	<section class="content">
+		<p class="title">Я готов заплатить:</p>
+		
+		<div class="inputs">
+			<input min={1000} max={2000} class:indigo={status == "normal"} class:green={status == "best"} class:red={status == "error"} class:orange={status == "low"} bind:value={amount} class="input" type="number" placeholder="Сумма..." /> 
+			<p class="currency">₽</p>
+		</div>
 
-		<div class="statistics">
-			<div class="benchmarks">
-				<p class="benchmark">{minPrice} ₽</p>
-				<p class="benchmark">{maxPrice} ₽</p>
-			</div>
+		<div class="statistics" class:best={status == "best"} class:normal={status == "normal"} class:error={status == 'error'}>
 
 			<div class="outer">
-				<div class:higher={amount < currentPrice} class="asking" style:width={`${$askingPriceWidth}%`}></div>
+				<div class:higher={amount <= currentPrice} class="asking" style:width={`${$askingPriceWidth}%`}></div>
 				<div class="current" style:width={`${$currentPriceWidth}%`}></div>
+				<div class="benchmarks">
+					<p class="benchmark">{minPrice} ₽</p>
+					<p class="benchmark">{maxPrice} ₽</p>
+				</div>
 			</div>
-			<div class="benchmarks">
-				<p class="benchmark">min. price</p>
-				<p class="benchmark">max. price</p>
-			</div>
+
 		</div>
 	</section>
 </main>
 
 <style>
+
 	@import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
 
 	* {
 		font-family: 'Inter', sans-serif;
 	}
 
+	.content {
+		min-width: 250px;;
+	}
+
 	.outer {
-		height: 15px;
+		height: 20px;
 		position: relative;
-		border-radius: 4px;
-		background-color: #D9D9D9;
+		border-radius: 6px;
+		background-color: rgb(255, 244, 230);
 	}
 
 	.current {
 		position: absolute;
-		height: 15px;
-		border-radius: 4px;
+		height: 20px;
+		border-radius: 6px;
 		z-index: 2;
-		background-color: #808080;
+		background-color: rgb(250, 176, 5);
 	}
 
 	.asking {
 		position: absolute;
 		z-index: 2;
-		height: 15px;
-		border-radius: 4px;
-		background-color: red;
+		height: 20px;
+		border-radius: 6px;
+		background-color: rgb(253, 126, 20);
+	}
+
+	.normal .outer {
+		background-color: rgb(237, 242, 255);
+	}
+
+	.normal .asking {
+		background-color: rgb(76, 110, 245);
+	}
+
+	.error .outer {
+		background-color: rgb(255, 245, 245);
+	}
+
+	.error .current {
+		background-color: rgb(250, 82, 82);
+	}
+
+	.benchmarks {
+		position: absolute;
+		display: flex;
+		justify-content: space-between;
+		width: 100%;
+		top: 0;
+		align-items: center;
+		height: 20px;
+		box-sizing: border-box;
+		padding: 0 6px;
+	}
+
+	.benchmark {
+		margin: 0;
+		font-size: 10px;
+		z-index: 3;
+		color: black;
 	}
 
 	.higher {
@@ -108,29 +152,39 @@
 		gap: 5px;
 	}
 
-	.benchmarks {
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.benchmark {
-		font-size: 12px;
-		margin: 0;
-	}
-
 	.title {
-		font-size: 24px;
-		font-weight: bolder;
+		margin: 0 0 10px 0;
+		font-size: 18px;
+	}
+
+	.inputs {
+		position: relative;
+		width: 100%;
+	}
+
+	.currency {
+		margin: 0;
+		position: absolute;
+		top: 50%;
+		transform: translate(0, -50%);
+		right: 12px;
 	}
 
 	.input {
-		padding: 20px;
+		width: 100%;
+		box-sizing: border-box;
+		color: black;
+		border: 2px solid rgb(206, 212, 218);
+		padding: 10px 10px;
 		border-radius: 12px;
-		
-		background-color: #D9D9D9;
 
-		border: none;
 		outline: none;
+
+		position: relative;
+	}
+
+	.input::placeholder {
+		color: grey;
 	}
 
 	input::-webkit-outer-spin-button,
@@ -142,6 +196,31 @@
 	/* Firefox */
 	input[type=number] {
 	  -moz-appearance: textfield;
+	}
+
+	.orange {
+		border: 2px solid rgb(253, 126, 20);
+	}
+
+	.red {
+		border: 2px solid rgb(250, 82, 82);
+	}
+
+	.green {
+		border: 2px solid rgb(18, 184, 134);
+	}
+	.indigo {
+		border: 2px solid rgb(76, 110, 245);
+	}
+
+	.best .outer {
+		background-color: rgb(230, 252, 245);
+	}
+	.best .current {
+		background-color: rgb(77, 220, 177);
+	}
+	.best .asking {
+		background-color: rgb(18, 184, 134);
 	}
 </style>
 
